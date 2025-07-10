406d16d5f635ec2cbb69e0067f3ca2de7bcf72ea
Adam zahi <zahi.adem10@gmail.com>
2025-07-10 15:54:14 +0100

    increase main content's width

--- app/home/layout.js
+++ app/home/layout.js
@@ -18,7 +18,7 @@ export default function LeadsLayout({ children }) {
           </div>
         </div>
       </header>
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</main>
+      <main className="max-full mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</main>
     </div>
   )
 }
